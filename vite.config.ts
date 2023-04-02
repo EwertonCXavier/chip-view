0a1386233c1cccb6cb72e19cac4092c7f2409b67
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 14:06:47 -0300

    chore: version update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/3D-View',
   build: {
     chunkSizeWarningLimit: 1600,
   },
