3247e4d9367669ce93fb059a2aa2ac3639eecd3f
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 15:26:14 -0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3D-View',
+  base: '/chip-view',
   build: {
     chunkSizeWarningLimit: 1600,
   },
