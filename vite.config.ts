f0a23d62bc006420ac815a09715281010e142d55
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 19:58:10 -0300

    fix: removing base name in vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chip-view/',
   build: {
     chunkSizeWarningLimit: 1600,
   },
