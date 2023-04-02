942438c3f6de082c3c1a4a8c1c4ed7b2d65b8a61
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 17:27:18 -0300

    refactor: view properties and style update

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function Model(props: any) {
 export const App = () => {
   return (
     <>
-      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: "absolute", width: "500px", height: "500px" }}>
-        <color attach="background" args={['#101010']} />
-        <PresentationControls speed={1.5} global zoom={0.5} polar={[-Math.PI, Math.PI]}>
-          <Stage environment={undefined}>
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }} style={{ position: "absolute", width: "100%", height: "100%" }}>
+        <color attach="background" args={['#333']} />
+        <PresentationControls speed={1.5} global zoom={1} polar={[-Math.PI, Math.PI]}>
+          <Stage environment={"sunset"}>
             <Model scale={1} />
           </Stage>
         </PresentationControls>
