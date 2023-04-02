bf2e47aa3806df2d6451cd5c3f257c65ff4d94b5
EwertonCXavier <ewertoncassianox@gmail.com>
2023-04-02 15:36:19 -0300

    chore: removing public folder and inserting assets one

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 
 
 function Model(props: any) {
-  const { scene } = useGLTF('/HTLRBR32L10_3D.glb');
+  const { scene } = useGLTF('./src/assets/HTLRBR32L10_3D.glb');
 
   return <primitive object={scene} {...props} />
 }
@@ -15,7 +15,7 @@ export const App = () => {
         <color attach="background" args={['#101010']} />
         <PresentationControls speed={1.5} global zoom={0.5} polar={[-Math.PI, Math.PI]}>
           <Stage environment={undefined}>
-            <Model scale={0.01} />
+            <Model scale={1} />
           </Stage>
         </PresentationControls>
       </Canvas>
